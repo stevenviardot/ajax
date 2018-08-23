e2a5ff9b1036e8a511d48d61665edf08156dffa3
steven@forban.io <steven@MacBook-Air-de-viardot.local>
2018-08-23 21:26:24 +0200

    final push

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,13 +1,17 @@
 class EmailController < ApplicationController
   def index
     @email = Email.all
-  end
-
-  def show
-    @email = Email.all
     respond_to do |format|
       format.html
       format.js
     end
   end
+
+
+  private
+
+  def email_params
+    params.require(:email).permit(:objet, :body)
+  end
+
 end
