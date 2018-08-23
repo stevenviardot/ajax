940478bb5e577808f15a1212220f192568cc7376
steven@forban.io <steven@MacBook-Air-de-viardot.local>
2018-08-23 17:53:11 +0200

    js alert test

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -2,4 +2,12 @@ class EmailController < ApplicationController
   def index
     @email = Email.all
   end
+
+  def show
+    @email = Email.all
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
 end
